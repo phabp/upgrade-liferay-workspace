2e8e84c5082f87f038001cde3417af2786bac905
phabp <paulo.pereira@liferay.com>
2026-01-06 11:33:39 -0300

    arquillian dependency inserted on modules/sample-rest-builder/buildgrade

--- modules/sample-rest-builder/sample-rest-builder-test/src/testIntegration/java/sample/rest/builder/resource/v1_0/test/BaseBarResourceTestCase.java
+++ modules/sample-rest-builder/sample-rest-builder-test/src/testIntegration/java/sample/rest/builder/resource/v1_0/test/BaseBarResourceTestCase.java
@@ -38,7 +38,7 @@ import java.text.Format;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
+import java.util.Date;		
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
